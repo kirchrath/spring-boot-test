e0a46689538946839257acd491f3ec70f01ffbe1
Chandra Kirchrath <chandra.kirchrath@linkmobility.com>
2019-02-05 22:33:39 +0100

    some more test

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mvn clean package
-docker build . -t=kirchrath/spring-boot-test
+docker build . -t=kirchrath/spring-boot-test:latest -t=kirchrath/spring-boot-test:$(date +%Y%m%d%H%M%S)
 docker push kirchrath/spring-boot-test
--- src/main/java/de/kirchrath/springtest/rest/TestController.java
+++ src/main/java/de/kirchrath/springtest/rest/TestController.java
@@ -5,6 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -31,5 +38,22 @@ public class TestController {
                 .filter(name -> !name.startsWith("."))
                 .collect(Collectors.toList());
         return filenames;
+    }
+
+    @GetMapping("/write")
+    public List<String> write() {
+        String filename = configuration.getDir() + "/bla";
+
+        try {
+            Files.write(Paths.get(filename), (getDateString() + "\n").getBytes(), StandardOpenOption.APPEND);
+            return Files.readAllLines(Paths.get(filename), Charset.defaultCharset());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getDateString() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return format.format(new Date());
     }
 }
